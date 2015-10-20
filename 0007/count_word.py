e3aa39e0a89c8a0370331fecbfb0f288f6116864
exia56 <exia5693@gmail.com>
2015-10-20 15:43:41 +0800

    test

--- 0007/count_word.py
+++ 0007/count_word.py
@@ -2,21 +2,28 @@ import sys
 import os
 import re
 
-PATH = "./"
+PATH = "0007/"
 words = {}
 regex = re.compile('[^a-zA-Z]')
 
 def main():
     path = os.listdir(PATH)
-    print __file__
-    print os.path.basename(sys.argv[0])
-    # for filename in path:
-    #     txt_name = PATH + filename
-    #     try :
-    #         input_txt = open(txt_name, 'r')
-    #     except :
-    #         print "Something Error"
-        
+    p = os.path.basename(sys.argv[0])
+    for filename in path:
+        if p in path:
+            path.remove(p)
+        txt_name = PATH + filename
+        if os.path.isdir(txt_name):
+            path.append(txt_name)
+
+        #try :
+        #    input_txt = open(txt_name, 'r')
+        #except :
+        #    print "Something Error"
+    
+
+def count_line_re():
+    pass
 
 
     
